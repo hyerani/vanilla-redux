5666d8a8391abe51cdab036371c80e4bacd5bcdd
hyerani <hran9404@naver.com>
2023-03-15 14:19:49 +0900

    feat: add count func

--- src/index.js
+++ src/index.js
@@ -1,1 +1,61 @@
+import { createStore } from "redux";
 
+const add = document.getElementById("add");
+const minus = document.getElementById("minus");
+const number = document.querySelector("span");
+
+number.innerText = 0;
+
+const ADD = "ADD";
+const MINUS = "MINUS";
+
+// data를 수정하는 한개의 function = reducer
+// return 값이 data 저장소 값이 됨
+const countModifier = (count = 0, action) => {
+  switch (action.type) {
+    case ADD:
+      return count + 1;
+    case MINUS:
+      return count - 1;
+    default:
+      return count;
+  }
+};
+
+// data 저장소 = store
+const countStore = createStore(countModifier);
+
+const onChange = () => {
+  number.innerText = countStore.getState();
+};
+
+countStore.subscribe(onChange);
+
+// dispatch의 객체 안에 type은 이름을 변경 할 수 없음
+add.addEventListener("click", () => {
+  countStore.dispatch({ type: ADD });
+});
+minus.addEventListener("click", () => {
+  countStore.dispatch({ type: MINUS });
+});
+
+// VANILLA JS로 구현한 카운터 기능
+// let count = 0;
+// number.innerText = count;
+
+// const updateText = () => {
+//   number.innerText = count;
+// };
+
+// const handleAdd = () => {
+//   count = count + 1;
+//   updateText();
+// };
+
+// const handleMinus = () => {
+//   count = count - 1;
+//   updateText();
+// };
+
+// add.addEventListener("click", handleAdd);
+// minus.addEventListener("click", handleMinus);
